401167da27994282eadbd3c66398551047eea089
Vladislav <91069241+Asphodell@users.noreply.github.com>
2023-03-26 23:27:13 +0300

    Added tests; fixed bugs

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "tic_tac_toe.h"
+#include <cassert>
 using namespace std;
 
 char board[3][3] = { {'1','2','3'},{'4','5','6'},{'7','8','9'} };
@@ -236,32 +236,32 @@ void markBoardAI(int pos)
     {
         switch (currentMove)
         {
-            case 2: {
-                switch (pos)
-                {
-                    case 2: board[1][2] = AI; break;
-                    case 4: board[0][1] = AI; break;
-                    case 6: board[2][1] = AI; break;
-                    case 8: board[1][0] = AI; break;
-                    default: randomMoveAI(); break;
-                }
-                currentMove += 1;
-                return;
+        case 2: {
+            switch (pos)
+            {
+            case 2: board[1][2] = AI; break;
+            case 4: board[0][1] = AI; break;
+            case 6: board[2][1] = AI; break;
+            case 8: board[1][0] = AI; break;
+            default: randomMoveAI(); break;
             }
-            case 3:
-                if (checkWinStrategyAI) return;
-                else
-                {
-                    randomMoveAI();
-                    currentMove += 1;
-                    break;
-                }
-            default:
+            currentMove += 1;
+            return;
+        }
+        case 3:
+            if (checkWinStrategyAI) return;
+            else
             {
                 randomMoveAI();
                 currentMove += 1;
                 break;
             }
+        default:
+        {
+            randomMoveAI();
+            currentMove += 1;
+            break;
+        }
         }
     }
     else
@@ -306,10 +306,10 @@ int main() {
         if (player == 'X') AI = 'O';
         else AI = 'X';
         if (AI == 'X')
-            {
-                board[1][1] = AI;
-                currentMove += 1;
-            }
+        {
+            board[1][1] = AI;
+            currentMove += 1;
+        }
         while (!gameOver)
         {
             int pos;
@@ -348,3 +348,42 @@ int main() {
     }
     return 0;
 }
+
+int test1() {
+    markBoard(1);
+    assert(board[0][0] == 'X');
+    togglePlayer();
+    assert(player == 'O');
+    return 0;
+}
+
+int test2() {
+    markBoard(1);
+    incorrect = false;
+    markBoard(1);
+    assert(incorrect == true);
+    return 0;
+}
+
+int test3() {
+    markBoard(1);
+    togglePlayer();
+    markBoard(2);
+    togglePlayer();
+    markBoard(3);
+    togglePlayer();
+    markBoard(4);
+    togglePlayer();
+    markBoard(6);
+    togglePlayer();
+    markBoard(5);
+    togglePlayer();
+    markBoard(7);
+    togglePlayer();
+    markBoard(9);
+    togglePlayer();
+    markBoard(8);
+    assert(checkTie() == true);
+    return 0;
+}
+

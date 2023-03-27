b7ef4022e78355b639b9804db2d897fe0d4a272f
Vladislav <91069241+Asphodell@users.noreply.github.com>
2023-03-27 10:53:31 +0300

    now without лишних функций

--- main.cpp
+++ main.cpp
@@ -1,11 +1,10 @@
-#include <iostream>
-#include <cassert>
-#include <tic_tac_toe.h>
-
+﻿#include <iostream>
+#include "GameEngine.h"
+#include "AI.h"
 using namespace std;
 
 char board[3][3] = { {'1','2','3'},{'4','5','6'},{'7','8','9'} };
-int possibleInput[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+int possibleInput[9] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 char player = 'X';
 bool gameOver = false;
 bool incorrect = false;
@@ -13,100 +12,6 @@ bool incorrect = false;
 char gameMode = 'P';
 char AI = 'O';
 char currentMove = '1';
-
-void drawBoard() {
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            cout << board[i][j] << " ";
-        }
-        cout << endl;
-    }
-}
-
-void togglePlayer() {
-    if (player == 'X') {
-        player = 'O';
-    }
-    else {
-        player = 'X';
-    }
-}
-
-bool checkWin(char player) {
-    for (int i = 0; i < 3; i++) {
-        if (board[i][0] == player && board[i][1] == player && board[i][2] == player) {
-            return true;
-        }
-    }
-
-    for (int j = 0; j < 3; j++) {
-        if (board[0][j] == player && board[1][j] == player && board[2][j] == player) {
-            return true;
-        }
-    }
-
-    if (board[0][0] == player && board[1][1] == player && board[2][2] == player) {
-        return true;
-    }
-
-    if (board[0][2] == player && board[1][1] == player && board[2][0] == player) {
-        return true;
-    }
-    return false;
-}
-
-void markBoard(int pos) {
-    if (pos == 1 && board[0][0] == '1') {
-        board[0][0] = player;
-    }
-
-    else if (pos == 2 && board[0][1] == '2') {
-        board[0][1] = player;
-    }
-
-    else if (pos == 3 && board[0][2] == '3') {
-        board[0][2] = player;
-    }
-
-    else if (pos == 4 && board[1][0] == '4') {
-        board[1][0] = player;
-    }
-
-    else if (pos == 5 && board[1][1] == '5') {
-        board[1][1] = player;
-    }
-
-    else if (pos == 6 && board[1][2] == '6') {
-        board[1][2] = player;
-    }
-
-    else if (pos == 7 && board[2][0] == '7') {
-        board[2][0] = player;
-    }
-
-    else if (pos == 8 && board[2][1] == '8') {
-        board[2][1] = player;
-    }
-
-    else if (pos == 9 && board[2][2] == '9') {
-        board[2][2] = player;
-    }
-    else {
-        incorrect = true;
-        std::cout << "Incorrect step!" << std::endl;
-    }
-}
-
-bool checkTie() {
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            if (board[i][j] != 'X' && board[i][j] != 'O') {
-                return false;
-            }
-        }
-    }
-    return true;
-}
 
 bool checkPossibleWin(char current)
 {
@@ -177,55 +82,6 @@ bool checkPossibleWin(char current)
     return false;
 }
 
-bool checkWinStrategyAI()
-{
-    if (board[1][1] == AI)
-    {
-        if (board[2][1] == AI && board[2][0] != player && board[2][2] != player && board[0][2] != player)
-        {
-            board[2][0] == AI;
-            currentMove += 1;
-            return true;
-        }
-        else if (board[1][2] == AI && board[2][2] != player && board[0][0] != player && board[0][2] != player)
-        {
-            board[2][2] == AI;
-            currentMove += 1;
-            return true;
-        }
-        else if (board[0][1] == AI && board[0][2] != player && board[0][0] != player && board[2][0] != player)
-        {
-            board[0][2] == AI;
-            currentMove += 1;
-            return true;
-        }
-        else if (board[1][0] == AI && board[0][0] != player && board[2][0] != player && board[2][2] != player)
-        {
-            board[0][2] == AI;
-            currentMove += 1;
-            return true;
-        }
-    }
-    return false;
-}
-
-void randomMoveAI()
-{
-    bool correctValue = false;
-    while (!correctValue)
-    {
-        srand(time(NULL));
-        int x = rand() % 3;
-        int y = rand() % 3;
-        if (board[x][y] != 'X' && board[x][y] != 'O')
-        {
-            board[x][y] = AI;
-            correctValue = true;
-            break;
-        }
-    }
-}
-
 void markBoardAI(int pos)
 {
     if (checkPossibleWin(AI)) return;
@@ -247,22 +103,22 @@ void markBoardAI(int pos)
             case 4: board[0][1] = AI; break;
             case 6: board[2][1] = AI; break;
             case 8: board[1][0] = AI; break;
-            default: randomMoveAI(); break;
+            default: randomMoveAI(board, AI); break;
             }
             currentMove += 1;
             return;
         }
         case 3:
-            if (checkWinStrategyAI) return;
+            if (checkWinStrategyAI(board, AI, currentMove, player)) return;
             else
             {
-                randomMoveAI();
+                randomMoveAI(board, AI);
                 currentMove += 1;
                 break;
             }
         default:
         {
-            randomMoveAI();
+            randomMoveAI(board, AI);
             currentMove += 1;
             break;
         }
@@ -270,20 +126,21 @@ void markBoardAI(int pos)
     }
     else
     {
-        randomMoveAI();
+        randomMoveAI(board, AI);
         currentMove += 1;
     }
     return;
 }
 
-int main() {
+int main()
+{
     cout << "Write 'P' to play with another player or 'B' to play with a bot" << endl;
     cin >> gameMode;
-    if (gameMode == 'P')
-    {
+
+    if (gameMode == 'P') {
         while (!gameOver) {
             int pos;
-            drawBoard();
+            drawBoard(board);
             cout << "Player " << player << ", enter a position (1-9): ";
             cin >> pos;
             bool correctInput = false;
@@ -301,20 +158,20 @@ int main() {
                 std::cout << "Incorrect step!" << std::endl;
                 return 1;
             }
-            markBoard(pos);
+            markBoard(pos, board, player);
 
-            if (checkWin(player)) {
-                drawBoard();
+            if (checkWin(player, board)) {
+                drawBoard(board);
                 cout << "Player " << player << " wins!" << endl;
                 gameOver = true;
             }
-            else if (checkTie()) {
-                drawBoard();
+            else if (checkTie(board)) {
+                drawBoard(board);
                 cout << "Tie game!" << endl;
                 gameOver = true;
             }
             else {
-                togglePlayer();
+                togglePlayer(player);
             }
         }
     }
@@ -332,77 +189,37 @@ int main() {
         while (!gameOver)
         {
             int pos;
-            drawBoard();
+            drawBoard(board);
             cout << "Player " << player << ", enter a position (1-9): ";
             cin >> pos;
-            markBoard(pos);
-            if (checkWin(player))
+            markBoard(pos, board, player);
+            if (checkWin(player, board))
             {
-                drawBoard();
+                drawBoard(board);
                 cout << "You win!" << endl;
                 gameOver = true;
             }
-            else if (checkTie()) {
-                drawBoard();
+            else if (checkTie(board)) {
+                drawBoard(board);
                 cout << "Tie game!" << endl;
                 gameOver = true;
             }
             markBoardAI(pos);
-            if (checkWin(AI))
+            if (checkWin(AI, board))
             {
-                drawBoard();
+                drawBoard(board);
                 cout << "You lose!" << endl;
                 gameOver = true;
             }
-            else if (checkTie()) {
-                drawBoard();
+            else if (checkTie(board)) {
+                drawBoard(board);
                 cout << "Tie game!" << endl;
                 gameOver = true;
             }
         }
     }
     else {
-        incorrect = true;
-        std::cout << "Incorrect step!" << std::endl;
+       std::cout << "Incorrect step!" << std::endl;
     }
     return 0;
 }
-
-int test1() {
-    markBoard(1);
-    assert(board[0][0] == 'X');
-    togglePlayer();
-    assert(player == 'O');
-    return 0;
-}
-
-int test2() {
-    markBoard(1);
-    incorrect = false;
-    markBoard(1);
-    assert(incorrect == true);
-    return 0;
-}
-
-int test3() {
-    markBoard(1);
-    togglePlayer();
-    markBoard(2);
-    togglePlayer();
-    markBoard(3);
-    togglePlayer();
-    markBoard(4);
-    togglePlayer();
-    markBoard(6);
-    togglePlayer();
-    markBoard(5);
-    togglePlayer();
-    markBoard(7);
-    togglePlayer();
-    markBoard(9);
-    togglePlayer();
-    markBoard(8);
-    assert(checkTie() == true);
-    return 0;
-}
-
